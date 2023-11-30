2b300773ab987d22235f8e56602b2ffdf4535634
Kvanp <lall@feinripptraeger.de>
2023-11-30 13:45:04 +0100

    Wrong user host syntax

--- windows/cmd_prompt.bat
+++ windows/cmd_prompt.bat
@@ -5,7 +5,7 @@ rem	My Command Line Prompt
 rem ****************************************************************************
 
 rem Set for current session
-set prompt=%COMPUTERNAME%@%USERNAME%$S$S$D$S$S$T$S$S$M$S$P$_$G$S
+set prompt=%USERNAME%@%COMPUTERNAME%$S$S$D$S$S$T$S$S$M$S$P$_$G$S
 
 rem For ever
 setx prompt %prompt%
